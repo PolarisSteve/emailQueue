489b330043dee898e562431dd6d7ce69fe58313c
Steven Contos <steve.contos@gmail.com>
2019-12-20 08:44:49 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emailQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EmailRoutine.SendEmail(args[0], args[1]);
+
+
+
+        }
+    }
+}
