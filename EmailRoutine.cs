0779c03cc9b02002c928e8979480f4e71e0a8480
Steven Contos <steve.contos@gmail.com>
2019-12-20 08:50:05 -0600

    For clarity wrapped disposable objects in using statements

--- EmailRoutine.cs
+++ EmailRoutine.cs
@@ -13,38 +13,40 @@ namespace emailQueue
 
         //Borrowed structure from https://docs.microsoft.com/en-us/dotnet/api/system.net.mail.smtpclient?view=netframework-4.8
 
-        
+
 
 
         public static void SendEmail(string To, string From)
         {
 
-            SmtpClient client = new SmtpClient("localhost");
-            // Specify the email sender.
-            // Create a mailing address that includes a UTF8 character
-            // in the display name.
-            MailAddress from = new MailAddress(From,
-               From,
-            System.Text.Encoding.UTF8);
-            // Set destinations for the email message.
-            MailAddress to = new MailAddress(To);
-            // Specify the message content.
-            MailMessage message = new MailMessage(from, to);
-            message.Body = "This is a test email message sent by an application. ";
-            // Include some non-ASCII characters in body and subject.
-            string someArrows = new string(new char[] { '\u2190', '\u2191', '\u2192', '\u2193' });
-            message.Body += Environment.NewLine + someArrows;
-            message.BodyEncoding = System.Text.Encoding.UTF8;
-            message.Subject = "test message 1" + someArrows;
-            message.SubjectEncoding = System.Text.Encoding.UTF8;
-            client.Send(message);
-            Console.WriteLine("Messgae Sent. Press any  key to exit.");
+            using (SmtpClient client = new SmtpClient("localhost"))
+            {
+                // Specify the email sender.
+                // Create a mailing address that includes a UTF8 character
+                // in the display name.
+                MailAddress from = new MailAddress(From,
+                   From,
+                System.Text.Encoding.UTF8);
+                // Set destinations for the email message.
+                MailAddress to = new MailAddress(To);
+                // Specify the message content.
 
-            string answer = Console.ReadLine();
-            // Clean up.
-            message.Dispose();
+                using (MailMessage message = new MailMessage(from, to))
+                {
+                    message.Body = "This is a test email message sent by an application. ";
+                    // Include some non-ASCII characters in body and subject.
+                    string someArrows = new string(new char[] { '\u2190', '\u2191', '\u2192', '\u2193' });
+                    message.Body += Environment.NewLine + someArrows;
+                    message.BodyEncoding = System.Text.Encoding.UTF8;
+                    message.Subject = "test message 1" + someArrows;
+                    message.SubjectEncoding = System.Text.Encoding.UTF8;
+                    client.Send(message);
+                    Console.WriteLine("Messgae Sent. Press any  key to exit.");
+
+                    string answer = Console.ReadLine();
+                }
+            }
             Console.WriteLine("Goodbye.");
-
 
         }
     
